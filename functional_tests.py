7ace4af6ee56b3bf1274e203f3db55e88b1ca747
Gray <heather.gray@hp.com>
2019-10-15 09:55:58 -0600

    check for 2nd list item test failing

--- functional_tests.py
+++ functional_tests.py
@@ -3,14 +3,21 @@ from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
+
 class NewVisitorTest(unittest.TestCase):
-    
+
     def setUp(self):
         self.browser = webdriver.Firefox()
-    
+
     def tearDown(self):
         self.browser.quit()
-    
+
+    # helper methods
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do
         # to check out is homepage
@@ -34,28 +41,26 @@ class NewVisitorTest(unittest.TestCase):
         # when she hits enter, the page updates, and now the page lists
         # 1: Buy peacock feathers as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        
         # there is still a textbox inviting her to add a new to-do
         # she enters "Use peacock feathers to make a fly"
-        self.fail('Finish the test!')
-        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        #The page updates again and now shows both items on her list
-
+        # The page updates again and now shows both items on her list
 
         # Will the site remember her list? She sees that the site has 
         # generated a unique url for her with some explanatory text
 
-
         # She visits that URL and her to-do list is still there
 
-
         # Satisfied, she closes the window
+
 
 if __name__ == "__main__":
     unittest.main(warnings='ignore')
